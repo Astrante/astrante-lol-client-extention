1191185ea49061909704f39b013b42b658b7c21c
Astrante <olga.ru97@mail.ru>
2026-02-09 22:12:18 +0100

    Update default settings

--- src/config/datastoreDefault.js
+++ src/config/datastoreDefault.js
@@ -1,8 +1,8 @@
 export default {
-    "auto_accept": false,
     "theme_enabled": true,
-    "hide_tft": true,
-    "hide_tft_mode": false,
-    "hide_tft_tab": false,
-    "hide_tft_mission": false,
+    "auto_accept": true,
+    "hide_tft": false,
+    "hide_tft_mode": true,
+    "hide_tft_tab": true,
+    "hide_tft_mission": true,
 }
